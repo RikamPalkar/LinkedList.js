2784496d27bb797aae2e979d75bcefdd9002da4c
Rikam <rikampalkar@gmail.com>
2022-02-06 21:55:18 +0530

    Adding Reverse LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -171,6 +171,27 @@ class LinkedList{
         //Return the array
         return printArrayList.join(' -> ');
     }
+
+    reverseList(){
+        if(!this.head || !this.head.next)
+        {
+            return this.head;
+        }
+        let previousNode = this.head;
+        let currentNode = previousNode.next;        
+        this.tail = this.head;
+        while(currentNode){
+
+            let nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+
+        }
+        this.head.next = null;
+        this.head = previousNode;
+        return this;
+    }
 }
 console.log('Creating a LinkList at constant time O(1): 20:');
 const myLinkedList = new LinkedList(20);
@@ -220,3 +241,58 @@ console.log(myLinkedList.printList());
 console.log(myLinkedList.searchElement(20));
 console.log('Searching for value 50 with time complexty of O(n):');
 console.log(myLinkedList.searchElement(50));
+
+
+console.log('Reverse LinkedList O(n):');
+console.log(myLinkedList.reverseList());
+console.log(myLinkedList.printList());
+console.log('');
+
+const myLinkedList1 = new LinkedList(10);
+myLinkedList1.append(20).append(30).append(40).append(50);
+
+function reverseLinkedList(linkedlist) {
+    if(!linkedlist){
+        return linkedlist;
+    }
+    var currentNode = linkedlist.head;
+    var previousNode = null;
+    var nextNode = null;
+    while(currentNode != null) {
+        /* 
+            This node will keep the track of third node.
+            Once we reverse the pointer of second element which is 20 in iteration 1, 10 <- 20,
+            We might lose third element to continue iteration, so keeping a track of next element.
+        */ 
+        nextNode = currentNode.next;
+
+        // We need reverse a pointer to point to the previous node. 10 <- 20
+        currentNode.next = previousNode;
+
+        /* 
+            Now we need to move to next 2 elements for comparision.
+            So increment previousNode to current node: 
+            previousNode = 20      
+        */
+            previousNode = currentNode;
+        
+        /*
+            Point current node to its next node,
+            currentNode = 30.
+        */
+        currentNode = nextNode;
+    }
+    return previousNode; 
+} 
+
+console.log('Creating a new LinkList at constant time O(1):');
+const myLinkedListTwo = new LinkedList(1);
+console.log(myLinkedListTwo.printList());
+console.log('');
+
+console.log('Appending Node at constant time O(1):');
+myLinkedListTwo.append(2).append(3).append(4).append(5);
+console.log(myLinkedListTwo.printList());
+console.log('');
+console.log('Reversing a linkedlist:');
+console.log(reverseLinkedList(myLinkedListTwo));
